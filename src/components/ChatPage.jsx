0350aa7a2aff464f35c275d54a2676ee6b7f0885
cioarauco <cioarauco@gmail.com>
2025-08-19 16:22:00 -0400

    Update ChatPage.jsx

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -8,7 +8,7 @@ import ChartInline from '../components/ChartInline';
 
 // 🌐 ENDPOINT DEL AGENTE
 const WEBHOOK_URL =
-  'https://n8n-production-6de7.up.railway.app/webhook-test/01103618-3424-4455-bde6-aa8d295157b2';
+  'https://n8n-production-6de7.up.railway.app/webhook/01103618-3424-4455-bde6-aa8d295157b2';
 
 export default function ChatPage() {
   const [input, setInput] = useState('');
